40b741c72b203f15102b7992511d64eff0cada9f
santiarr <santiagoarredondocif@gmail.com>
2020-03-29 01:28:43 -0400

    Click and open gifs/stickers + copy links

--- popup.js
+++ popup.js
@@ -8,6 +8,8 @@ let nextTextSelected = 1;
 let timesCalled = 1;
 let currentKeyword = 'hello';
 let currentPage = 0;
+let gifOpened = false;
+let toggleInnerHTML = '';
 
 
 loadTrending(60, false);
@@ -260,7 +262,6 @@ async function searchGif(keyword, limit, clear) {
 
 
 var base = 0;
-
 function renderImage(imageData, clear, keyword) {
 
   let offSet = 20;
@@ -273,21 +274,22 @@ function renderImage(imageData, clear, keyword) {
   }
   for (var i = base; i < base + offSet; i++) {
     document.getElementById('gifs-grid').innerHTML += `<div class="gif">
-  <img id=${i} src=${imageData.data[i].images.original.url} width="145" > </div>`
-  }
+  <img data-is-verified = "${imageData.data[i].user ? imageData.data[i].user.is_verified: 'nothing'}" data-avatar-url = "${imageData.data[i].user ? imageData.data[i].user.avatar_url: 'nothing' }" data-user-url = "${imageData.data[i].user ? imageData.data[i].user.profile_url : 'nothing' }" data-username = "${imageData.data[i].user ? imageData.data[i].user.username : 'nothing'}" data-display = "${imageData.data[i].user ? imageData.data[i].user.display_name :  'nothing'}"   class='giphy-image ${imageData.data[i].images.original.url} ${imageData.data[i].bitly_gif_url}' id=${i} src=${imageData.data[i].images.fixed_height_downsampled.url} width="145" ></div>`
+}
+
   base += offSet
   for (var i = base; i < base + offSet; i++) {
     document.getElementById('gifs-grid2').innerHTML += `<div class="gif">
-  <img id=${i} src=${imageData.data[i].images.original.url} width="145"> </div>`
+  <img data-is-verified = "${imageData.data[i].user ? imageData.data[i].user.is_verified: 'nothing'}" data-avatar-url = "${imageData.data[i].user ? imageData.data[i].user.avatar_url: 'nothing' }" data-user-url = "${imageData.data[i].user ? imageData.data[i].user.profile_url : 'nothing' }" data-username = "${imageData.data[i].user ? imageData.data[i].user.username : 'nothing'}" data-display = "${imageData.data[i].user ? imageData.data[i].user.display_name :  'nothing'}"   class='giphy-image ${imageData.data[i].images.original.url} ${imageData.data[i].bitly_gif_url}'  id=${i} src=${imageData.data[i].images.fixed_height_downsampled.url} width="145"></div>`
   }
   base += offSet
   for (var i = base; i < base + offSet; i++) {
     document.getElementById('gifs-grid3').innerHTML += `<div class="gif">
-  <img id=${i} src=${imageData.data[i].images.original.url} width="145" > </div>`
+  <img data-is-verified = "${imageData.data[i].user ? imageData.data[i].user.is_verified: 'nothing'}" data-avatar-url = "${imageData.data[i].user ? imageData.data[i].user.avatar_url: 'nothing' }" data-user-url = "${imageData.data[i].user ? imageData.data[i].user.profile_url : 'nothing' }" data-username = "${imageData.data[i].user ? imageData.data[i].user.username : 'nothing'}" data-display = "${imageData.data[i].user ? imageData.data[i].user.display_name :  'nothing'}"    class='giphy-image ${imageData.data[i].images.original.url} ${imageData.data[i].bitly_gif_url}' id=${i} src=${imageData.data[i].images.fixed_height_downsampled.url} width="145" ></div>`
   }
   base += offSet
   timesCalled += 1;
-
+ 
 }
 
 function renderTrending(imageData, clear) {
@@ -303,17 +305,17 @@ function renderTrending(imageData, clear) {
 
   for (var i = base; i < base + offSet; i++) {
     document.getElementById('gifs-grid').innerHTML += `<div class="gif">
-  <img id=${i} src=${imageData.data[i].images.original.url} width="145" > </div>`
+  <img data-is-verified = "${imageData.data[i].user ? imageData.data[i].user.is_verified: 'nothing'}" data-avatar-url = "${imageData.data[i].user ? imageData.data[i].user.avatar_url: 'nothing' }" data-user-url = "${imageData.data[i].user ? imageData.data[i].user.profile_url : 'nothing' }" data-username = "${imageData.data[i].user ? imageData.data[i].user.username : 'nothing'}" data-display = "${imageData.data[i].user ? imageData.data[i].user.display_name :  'nothing'}"   class='giphy-image ${imageData.data[i].images.original.url} ${imageData.data[i].bitly_gif_url}' id=${i} src=${imageData.data[i].images.fixed_height_downsampled.url} width="145" ></div>`
   }
   base += offSet
   for (var i = base; i < base + offSet; i++) {
     document.getElementById('gifs-grid2').innerHTML += `<div class="gif">
-  <img id=${i} src=${imageData.data[i].images.original.url} width="145"> </div>`
+  <img data-is-verified = "${imageData.data[i].user ? imageData.data[i].user.is_verified: 'nothing'}" data-avatar-url = "${imageData.data[i].user ? imageData.data[i].user.avatar_url: 'nothing' }" data-user-url = "${imageData.data[i].user ? imageData.data[i].user.profile_url : 'nothing' }" data-username = "${imageData.data[i].user ? imageData.data[i].user.username : 'nothing'}" data-display = "${imageData.data[i].user ? imageData.data[i].user.display_name :  'nothing'}"    class='giphy-image ${imageData.data[i].images.original.url} ${imageData.data[i].bitly_gif_url}' id=${i} src=${imageData.data[i].images.fixed_height_downsampled.url} width="145">  </div>`
   }
   base += offSet
   for (var i = base; i < base + offSet; i++) {
     document.getElementById('gifs-grid3').innerHTML += `<div class="gif">
-  <img id=${i} src=${imageData.data[i].images.original.url} width="145" > </div>`
+  <img data-is-verified = "${imageData.data[i].user ? imageData.data[i].user.is_verified: 'nothing'}" data-avatar-url = "${imageData.data[i].user ? imageData.data[i].user.avatar_url: 'nothing' }" data-user-url = "${imageData.data[i].user ? imageData.data[i].user.profile_url : 'nothing' }" data-username = "${imageData.data[i].user ? imageData.data[i].user.username : 'nothing'}" data-display = "${imageData.data[i].user ? imageData.data[i].user.display_name :  'nothing'}"    class='giphy-image ${imageData.data[i].images.original.url} ${imageData.data[i].bitly_gif_url}' id=${i} src=${imageData.data[i].images.fixed_height_downsampled.url} width="145" > </div>`
   }
   base += offSet
   timesCalled += 1;
@@ -384,17 +386,17 @@ function renderSticker(imageData, clear, keyword) {
   }
   for (var i = base; i < base + offSet; i++) {
     document.getElementById('gifs-grid').innerHTML += `<div class="gif">
-<img id=${i} src=${imageData.data[i].images.original.url} width="145" > </div>`
+<img data-is-verified = "${imageData.data[i].user ? imageData.data[i].user.is_verified: 'nothing'}" data-avatar-url = "${imageData.data[i].user ? imageData.data[i].user.avatar_url: 'nothing' }" data-user-url = "${imageData.data[i].user ? imageData.data[i].user.profile_url : 'nothing' }" data-username = "${imageData.data[i].user ? imageData.data[i].user.username : 'nothing'}" data-display = "${imageData.data[i].user ? imageData.data[i].user.display_name :  'nothing'}"   class='giphy-image ${imageData.data[i].images.original.url} ${imageData.data[i].bitly_gif_url}' id=${i} src=${imageData.data[i].images.fixed_height_downsampled.url} width="145" > </div>`
   }
   base += offSet
   for (var i = base; i < base + offSet; i++) {
-    document.getElementById('gifs-grid2').innerHTML += `<div class="gif">
-<img id=${i} src=${imageData.data[i].images.original.url} width="145"> </div>`
+    document.getElementById('gifs-grid2').innerHTML += `<div class="gif"> 
+<img data-is-verified = "${imageData.data[i].user ? imageData.data[i].user.is_verified: 'nothing'}" data-avatar-url = "${imageData.data[i].user ? imageData.data[i].user.avatar_url: 'nothing' }" data-user-url = "${imageData.data[i].user ? imageData.data[i].user.profile_url : 'nothing' }" data-username = "${imageData.data[i].user ? imageData.data[i].user.username : 'nothing'}" data-display = "${imageData.data[i].user ? imageData.data[i].user.display_name :  'nothing'}"   class='giphy-image ${imageData.data[i].images.original.url} ${imageData.data[i].bitly_gif_url}' id=${i} src=${imageData.data[i].images.fixed_height_downsampled.url} width="145">  </div>`
   }
   base += offSet
   for (var i = base; i < base + offSet; i++) {
     document.getElementById('gifs-grid3').innerHTML += `<div class="gif">
-<img id=${i} src=${imageData.data[i].images.original.url} width="145" > </div>`
+<img data-is-verified = "${imageData.data[i].user ? imageData.data[i].user.is_verified: 'nothing'}" data-avatar-url = "${imageData.data[i].user ? imageData.data[i].user.avatar_url: 'nothing' }" data-user-url = "${imageData.data[i].user ? imageData.data[i].user.profile_url : 'nothing' }" data-username = "${imageData.data[i].user ? imageData.data[i].user.username : 'nothing'}" data-display = "${imageData.data[i].user ? imageData.data[i].user.display_name :  'nothing'}"   class='giphy-image ${imageData.data[i].images.original.url} ${imageData.data[i].bitly_gif_url}' id=${i} src=${imageData.data[i].images.fixed_height_downsampled.url} width="145" ></div>`
   }
   base += offSet
   timesCalled += 1;
@@ -414,17 +416,19 @@ function renderTrendingSticker(imageData, clear) {
 
   for (var i = base; i < base + offSet; i++) {
     document.getElementById('gifs-grid').innerHTML += `<div class="gif">
-  <img id=${i} src=${imageData.data[i].images.original.url} width="145" > </div>`
+  <img data-is-verified = "${imageData.data[i].user ? imageData.data[i].user.is_verified: 'nothing'}" data-avatar-url = "${imageData.data[i].user ? imageData.data[i].user.avatar_url: 'nothing' }" data-user-url = "${imageData.data[i].user ? imageData.data[i].user.profile_url : 'nothing' }" data-username = "${imageData.data[i].user ? imageData.data[i].user.username : 'nothing'}" data-display = "${imageData.data[i].user ? imageData.data[i].user.display_name :  'nothing'}"  class='giphy-image ${imageData.data[i].images.original.url} ${imageData.data[i].bitly_gif_url}' id=${i} src=${imageData.data[i].images.fixed_height_downsampled.url} width="145"></div> 
+  </div>`
   }
   base += offSet
   for (var i = base; i < base + offSet; i++) {
     document.getElementById('gifs-grid2').innerHTML += `<div class="gif">
-  <img id=${i} src=${imageData.data[i].images.original.url} width="145"> </div>`
+  <img data-is-verified = "${imageData.data[i].user ? imageData.data[i].user.is_verified: 'nothing'}" data-avatar-url = "${imageData.data[i].user ? imageData.data[i].user.avatar_url: 'nothing' }" data-user-url = "${imageData.data[i].user ? imageData.data[i].user.profile_url : 'nothing' }" data-username = "${imageData.data[i].user ? imageData.data[i].user.username : 'nothing'}" data-display = "${imageData.data[i].user ? imageData.data[i].user.display_name :  'nothing'}"   class='giphy-image ${imageData.data[i].images.original.url} ${imageData.data[i].bitly_gif_url}' id=${i} src=${imageData.data[i].images.fixed_height_downsampled.url} width="145"></div> 
+  </div>`
   }
   base += offSet
   for (var i = base; i < base + offSet; i++) {
     document.getElementById('gifs-grid3').innerHTML += `<div class="gif">
-  <img id=${i} src=${imageData.data[i].images.original.url} width="145" > </div>`
+  <img data-is-verified = "${imageData.data[i].user ? imageData.data[i].user.is_verified: 'nothing'}" data-avatar-url = "${imageData.data[i].user ? imageData.data[i].user.avatar_url: 'nothing' }" data-user-url = "${imageData.data[i].user ? imageData.data[i].user.profile_url : 'nothing' }" data-username = "${imageData.data[i].user ? imageData.data[i].user.username : 'nothing'}" data-display = "${imageData.data[i].user ? imageData.data[i].user.display_name :  'nothing'}"   class='giphy-image ${imageData.data[i].images.original.url} ${imageData.data[i].bitly_gif_url}' id=${i} src=${imageData.data[i].images.fixed_height_downsampled.url} width="145" ></div>`
   }
   base += offSet
   timesCalled += 1;
@@ -502,13 +506,120 @@ document.getElementById('saved-text').addEventListener('click', savedClicked);
 
 /*Check if toggle elements were clicked*/
 
-document.getElementsByClassName('toggle-text')[0].addEventListener('click', function () { toggleClicked(0) });
-document.getElementsByClassName('toggle-text')[1].addEventListener('click', function () { toggleClicked(1) });
-document.getElementsByClassName('toggle-text')[2].addEventListener('click', function () { toggleClicked(2) });
-document.getElementsByClassName('toggle-text')[3].addEventListener('click', function () { toggleClicked(3) });
-document.getElementsByClassName('toggle-text')[4].addEventListener('click', function () { toggleClicked(4) });
-document.getElementsByClassName('toggle-text')[5].addEventListener('click', function () { toggleClicked(5) });
+document.getElementsByClassName('toggle-text')[0].addEventListener('click', function () { if(!gifOpened){toggleClicked(0)}});
+document.getElementsByClassName('toggle-text')[1].addEventListener('click', function () { if(!gifOpened){toggleClicked(1)}});
+document.getElementsByClassName('toggle-text')[2].addEventListener('click', function () { if(!gifOpened){toggleClicked(2)}});
+document.getElementsByClassName('toggle-text')[3].addEventListener('click', function () { if(!gifOpened){toggleClicked(3)}});
+document.getElementsByClassName('toggle-text')[4].addEventListener('click', function () { if(!gifOpened){toggleClicked(4)}});
+document.getElementsByClassName('toggle-text')[5].addEventListener('click', function () { if(!gifOpened){toggleClicked(5)}});
 
 
 
 document.getElementsByClassName('arrow-svg')[0].addEventListener('click', toggleAnimation);
+
+
+
+
+
+function removeElement(elementId) {
+  // Removes an element from the document
+  var element = document.getElementById(elementId);
+  element.parentNode.removeChild(element);
+}
+
+function addElement(parentId, elementTag, elementId, elementClass, elementWidth, html) {
+  // Adds an element to the document
+  var p = document.getElementById(parentId);
+  var newElement = document.createElement(elementTag);
+  newElement.setAttribute('id', elementId);
+  newElement.setAttribute('class', elementId);
+  newElement.setAttribute('width', elementWidth);
+  newElement.innerHTML = html;
+  p.appendChild(newElement);
+}
+
+
+/*Event bubbling !!*/
+document.addEventListener('click', function () {
+  if ( event.target.classList.contains('giphy-image' )) {
+      displayGif(event.target.src);
+  } else if ( event.target.id === 'back-button' ){
+      exit()
+  } else if (event.target.classList.contains('copy-link')){
+    copyToClipboard(event.target.getAttribute('data-link'));
+  }
+});
+
+
+
+
+function displayGif (url){  
+toggleInnerHTML = document.getElementById('toggle').innerHTML;
+removeElement('toggle');
+
+  document.getElementById('content-opened').innerHTML = `<div id="gif-opened">
+  <div id = 'gif-opened-header'>
+    <div id ='drag-dialogue' style="z-idex=10;">
+      <b>Drag and Drop</b>
+      &nbsp;GIF onto any web page
+    </div>
+    <img id ='back-button' src="static/back-button.png">
+  </div>
+  <div id="opened-gif">
+<a id="a-tag" href=${event.target.classList[2]} class = "opened-content-gif" style="background-color: rgb(16,16,16);"><img id="these-dimensions" class="opened-content-gif" src=${event.target.classList[1]}>
+    </a>
+  </div>
+  <div id='wrapsidk'>
+    <div id="gif-footer">
+      <img src=${event.target.getAttribute('data-avatar-url')!=='nothing'? event.target.getAttribute('data-avatar-url'): 'static/none.png' }>
+      <div id="username">
+        <div class="sc-htoDjs kgSoAQ">${event.target.getAttribute('data-display')!=='nothing' ? event.target.getAttribute('data-display'): ''}</div>
+        <a href="${event.target.getAttribute('data-user-url')!=='nothing' ? event.target.getAttribute('data-user-url'): ''}" target="_blank" rel="noopener noreferrer" class="at">${event.target.getAttribute('data-username')!=='nothing' ? '@' + event.target.getAttribute('data-username'): ''}&nbsp;${event.target.getAttribute('data-is-verified')!=='nothing'? '<svg viewBox="-1 0 18 18" version="1.1" width="14px"><g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd"><g id="Artboard-2" transform="translate(-324.000000, -132.000000)" fill="#15CDFF"><g id="Group-3" transform="translate(323.000000, 132.000000)"><polygon id="sprocket" points="8.95093746 16.1755971 6.18494567 16.8455287 4.34519872 14.6949378 1.70947694 13.628319 1.49869564 10.8185214 2.85993451e-13 8.42276436 1.49869564 6.02700728 1.70947694 3.21720971 4.34519872 2.1505909 6.18494567 4.92383911e-14 8.95093746 0.669931593 11.7169293 4.82947016e-14 13.5566762 2.1505909 16.192398 3.21720971 16.4031793 6.02700728 17.9018749 8.42276436 16.4031793 10.8185214 16.192398 13.628319 13.5566762 14.6949378 11.7169293 16.8455287"></polygon><path d="M9.32727273,9.44126709 L9.32727273,3.03016561 L6.55027155,3.03016561 L6.55027155,10.8150746 L6.55027155,12.188882 L12.1042739,12.188882 L12.1042739,9.44126709 L9.32727273,9.44126709 Z" id="check" fill="#121212" transform="translate(9.327273, 7.609524) scale(-1, 1) rotate(-45.000000) translate(-9.327273, -7.609524) "></path></g></g></g></svg>': '<div></div>'}</a>
+      </div>
+    </div>
+    <div class="copy-link" data-link ="${event.target.classList[2]}"id="not-copied">Copy Link</div>
+  </div>
+</div>
+`
+
+
+
+document.getElementById('a-tag').width=document.getElementById('these-dimensions').style.width
+document.getElementById('a-tag').height=document.getElementById('these-dimensions').style.height
+
+}
+ 
+
+ function exit(){
+   addElement('toggle-goes-here','div', 'toggle', 'toggle', '455',toggleInnerHTML)
+   document.getElementsByClassName('toggle-text')[0].addEventListener('click', function () { if(!gifOpened){toggleClicked(0)}});
+document.getElementsByClassName('toggle-text')[1].addEventListener('click', function () { if(!gifOpened){toggleClicked(1)}});
+document.getElementsByClassName('toggle-text')[2].addEventListener('click', function () { if(!gifOpened){toggleClicked(2)}});
+document.getElementsByClassName('toggle-text')[3].addEventListener('click', function () { if(!gifOpened){toggleClicked(3)}});
+document.getElementsByClassName('toggle-text')[4].addEventListener('click', function () { if(!gifOpened){toggleClicked(4)}});
+document.getElementsByClassName('toggle-text')[5].addEventListener('click', function () { if(!gifOpened){toggleClicked(5)}});
+document.getElementsByClassName('arrow-svg')[0].addEventListener('click', toggleAnimation);
+  document.getElementById('content-opened').innerHTML = '';
+
+ }
+
+
+ function copyToClipboard(textToCopy) {
+  var button = document.getElementsByClassName('copy-link')[0];
+  var textArea = document.createElement("textarea");
+  textArea.value = textToCopy;
+  button.appendChild(textArea);
+  textArea.select();
+  document.execCommand('copy');
+  button.removeChild(textArea);
+  button.id="copied"
+  button.innerHTML='Copied!'
+  setTimeout(notCopied,4500);
+}
+
+function notCopied (){
+  var button = document.getElementsByClassName('copy-link')[0];
+  button.innerHTML='Copy Link'
+  button.id="not-copied-b"
+  
+}
